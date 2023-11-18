a639d925810bb9b74fc6b7273491c65e51d986f4
Paul Brassington <brassington22@gmail.com>
2023-11-18 13:54:23 +0800

    start of using shared preferences rather than sqlite, big change comming, why? So it becomes web, windows and android

--- lib/domain/entities/accounts/account.dart
+++ lib/domain/entities/accounts/account.dart
@@ -1,14 +1,18 @@
+import 'dart:convert';
 import 'cash_action.dart';
 import 'cash_item.dart';
 import 'account_balance_at.dart';
 import '../../../../presentation/screens/paint/bar_loader.dart';
 import '../../../../presentation/config/constants.dart';
 
+List<Account> accountModelFromJson(String str) => List<Account>.from(json.decode(str)["notLearnt"].map((x) => Account.fromJson(x)));
+String accontModelToJson(List<Account> data) => json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
 class Account {
-  int id;
-  String accountName;
-  String description;
-  double balance;
+  late int id;
+  late String accountName;
+  late String description;
+  late double balance;
   bool usedForCashFlow = true;
   List<CashAction> cashActions = [];
   List<CashItem> cashFlow = [];
@@ -22,6 +26,23 @@ class Account {
     required this.balance,
     this.usedForCashFlow = true,
   });
+
+  Account.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    accountName = json['accountName'];
+    balance = json['balance'];
+    usedForCashFlow = json['usedForCashFlow'];
+  }
+  Map<String, dynamic> toJson() {
+    // not finished
+    final data = <String, dynamic>{};
+    data['"id"'] = '"$id"';
+    data['"accountName"'] = '"$accountName"';
+    data['"description"'] = '"$description"';
+    data['"balance"'] = '"$balance"';
+    return data;
+  }
+
   Account.startUp({this.id = AppConstants.createIDConstant, this.accountName = '', this.description = '', this.balance = 0.0});
   Account.noAccount({this.id = AppConstants.noAccount, this.accountName = '', this.description = '', this.balance = 0.0});
   bool isNoAccount() {
